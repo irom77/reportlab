7000ffbb48788e962e65ea1b1d9c98f8ebf0171d
irom77 <irek.romaniuk@gmail.com>
2024-08-28 17:49:39 -0400

    fix: Update threat_report.py configuration file path

--- threat_report.py
+++ threat_report.py
@@ -1,15 +1,15 @@
-from report import PDFGenerator # type: ignore
+# from report import PDFGenerator # type: ignore
 from connect import ProofpointAPIClient # type: ignore
 from xmltags import replace_fstr # type: ignore
-from figures import figure1, figure2  # type: ignore
+# from figures import figure1, figure2  # type: ignore
 from omegaconf import OmegaConf # type: ignore
 from datetime import datetime
 import sys
 
 
 if __name__ == "__main__":
-    conf = OmegaConf.load('configs/threat_report.yml')
-    print(conf.report.author, conf.report.api)
+    conf = OmegaConf.load('configs/_threat_report.yml')
+    print(conf.report.month)
     formatted_date=datetime.now().strftime('%B %d, %Y')
     client = ProofpointAPIClient()
     # result=client.messages_blocked()
